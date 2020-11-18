9e93c0ee7501dc24ea58c437debf55638cd620b1
Mona Lisa <patrinoua@gmail.com>
2020-11-19 00:12:45 +0100

    remove footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import MarketList from './components/MarketList';
-// import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <MarketList />
-      {/* <Footer /> */}
     </div>
   );
 }
